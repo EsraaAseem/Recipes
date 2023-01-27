b2994af2fa0dc9d84d24b4eda2ed0a97f2defda7
Esraa Aseem <114035531+EsraaAseem@users.noreply.github.com>
2023-01-27 20:29:45 +0200

    Update data-storage.service.ts

--- src/app/share/data-storage.service.ts
+++ src/app/share/data-storage.service.ts
@@ -13,15 +13,16 @@ export class DataStorageService {
   constructor(private hhtpClient:HttpClient,private recipeService:RecipeService,private authService:AuthService) { }
   StorageDate(){
     let recipes=this.recipeService.getRecipes();
-    
-    
-  this.hhtpClient.put('https://recipes-project-b75d8-default-rtdb.firebaseio.com/recipes.json',recipes)
-    .subscribe(response=>{console.log(response)})
-
+     
+  return this.authService.user.pipe(take(1),exhaustMap(user=>{
+   return this.hhtpClient.put<Recipe>('https://recipes-project-b75d8-default-rtdb.firebaseio.com/recipes.json',recipes,
+    {
+      params:new HttpParams().set('auth',user.token)}
+      )}))
   }
  FetchData(){
 
-    return this.hhtpClient.get<Recipe[]>('https://recipes-project-b75d8-default-rtdb.firebaseio.com/recipes.json')
+   /* return this.hhtpClient.get<Recipe[]>('https://recipes-project-b75d8-default-rtdb.firebaseio.com/recipes.json')
    .pipe(map(recipes=>{
     return recipes.map(recipe=>{
      return {...recipe,ingredients:recipe.ingredients?recipe.ingredients:[]};
@@ -29,9 +30,9 @@ export class DataStorageService {
 }),
 tap(recipes=>{
  this.recipeService.setRecipe(recipes);
-}))
+}))*/
  
-  /*return  this.authService.user.pipe(take(1),exhaustMap(user=>{
+  return  this.authService.user.pipe(take(1),exhaustMap(user=>{
       return this.hhtpClient.get<Recipe[]>('https://recipes-project-b75d8-default-rtdb.firebaseio.com/recipes.json',{
         params:new HttpParams().set('auth',user.token)
       })
@@ -43,7 +44,7 @@ tap(recipes=>{
  tap(recipes=>{
    this.recipeService.setRecipe(recipes);
  }))
-   */
+   
    
   }
 }
